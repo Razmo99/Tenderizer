db77b67a061cf0bede958d5befebd20c0a1c9298
Razmo99 <lewis.munro@live.com>
2021-06-16 12:59:23 +1000

    Context Menu for Tree View
    - Added right Click menu.
     - Delete selection
    Stuck the load and convert buttons in frames.
     - This allows for other buttons to be added from master classes easily.

--- Tenderizer/app_tk_widgets/components/treeview.py
+++ Tenderizer/app_tk_widgets/components/treeview.py
@@ -7,6 +7,12 @@ import tkinter.ttk as ttk
 
 logger = logging.getLogger(__name__)
 
+class RightClickSelection(tk.Menu):
+    """" Displays a menu with options for when multiple items are selected """
+    def __init__(self, master):
+        tk.Menu.__init__(self,master,tearoff=False)
+        self.add_command(label='Delete Selection',command=lambda :self.master.delete_selection())
+
 class TreeView(ttk.LabelFrame):
 
     def __init__(self, master,lf_title,column):
@@ -16,17 +22,17 @@ class TreeView(ttk.LabelFrame):
         self.grid_rowconfigure(1,weight=1)
         self.grid_columnconfigure(0,weight=1)
         # Hold the Treeview and scroll bar. Keeps them together
-        self.container=ttk.Frame(self)
-        self.container.grid(sticky='nsew',pady=5,padx=5,row=1,column=0,ipady=5,ipadx=5)
+        self.tv_frame=ttk.Frame(self)
+        self.tv_frame.grid(sticky='nsew',pady=5,padx=5,row=1,column=0,ipady=5,ipadx=5)
         # Make the tree ciew have priority for screen resizing
-        self.container.grid_rowconfigure(0,weight=0)
-        self.container.grid_rowconfigure(1,weight=1)
-        self.container.grid_rowconfigure(2,weight=0)
-        self.container.grid_columnconfigure(0,weight=1)
+        self.tv_frame.grid_rowconfigure(0,weight=0)
+        self.tv_frame.grid_rowconfigure(1,weight=1)
+        self.tv_frame.grid_rowconfigure(2,weight=0)
+        self.tv_frame.grid_columnconfigure(0,weight=1)
 
-        self.tree = ttk.Treeview(self.container,columns=column,show='headings')
+        self.tree = ttk.Treeview(self.tv_frame,columns=column,show='headings')
         # Scroll bar for tree view
-        self.scrollbar = ttk.Scrollbar(self.container, orient=tk.VERTICAL, command=self.tree.yview)
+        self.scrollbar = ttk.Scrollbar(self.tv_frame, orient=tk.VERTICAL, command=self.tree.yview)
         self.tree.configure(yscroll=self.scrollbar.set)
         self.scrollbar.grid(row=1, column=1,sticky='nsew')
 
@@ -36,11 +42,25 @@ class TreeView(ttk.LabelFrame):
         self.tree.grid(row=1,column=0,sticky='nsew')
 
         # Button to load Directory
-        self.load_button= ttk.Button(self,text='Load',command=self.master.load_pdfs,state='disabled')
+        self.load_frame = ttk.Frame(self)
+        self.load_frame.grid(sticky='nw',row=0,column=0)
+        self.load_frame.grid_rowconfigure(1,weight=1)
+        self.load_frame.grid_columnconfigure(0,weight=1)
+
+        self.load_button = ttk.Button(self.load_frame,text='Load',state='disabled')
         self.load_button.grid(row=0,column=0,sticky='nw',padx=5,pady=5)
         # Button to Convert Directory
-        self.convert_button=ttk.Button(self,text='Convert',command=self.master.convert_paths,state='disabled')
-        self.convert_button.grid(row=2,column=0,padx=5,pady=5,sticky='nw')        
+        self.converted_frame = ttk.Frame(self)
+        self.converted_frame.grid(sticky='nw',row=2,column=0)
+        self.converted_frame.grid_rowconfigure(1,weight=1)
+        self.converted_frame.grid_columnconfigure(0,weight=1)
+
+        self.convert_button=ttk.Button(self.converted_frame,text='Convert',state='disabled')
+        self.convert_button.grid(row=2,column=0,padx=5,pady=5,sticky='nw')
+        
+        self.right_click_selection_menu = RightClickSelection(self)
+        # When an item is right clicked bring up a menu
+        self.tree.bind('<Button-3>', self.right_click)     
 
     def sort_column(self,tv, col, reverse):
         """ Sorts Columns in a tree view"""
@@ -55,3 +75,26 @@ class TreeView(ttk.LabelFrame):
         # reverse sort next time
         tv.heading(col, text=col, command=lambda _col=col: \
                     self.sort_column(tv, _col, not reverse))
+    
+    def right_click(self, event):
+        selection = self.tree.selection()
+        if selection:
+            self.right_click_selection_menu.post(event.x_root,event.y_root)         
+        else:
+            pass
+
+    def delete_selection(self):
+        # list of pdf data classes
+        dataset = self.master.dataset
+        # current selection in the tree view
+        selection = self.tree.selection()
+        # iterate over the current selection
+        for iid in selection:
+            # iterate over the data set
+            for index,pdf in enumerate(dataset):
+                # look for an ID match
+                if iid == pdf.id:
+                    # remove the item from the dataset
+                    del dataset[index]
+        # Remove the selection from the tree view
+        self.tree.delete(*self.tree.selection())

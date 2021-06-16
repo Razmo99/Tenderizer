310373a2bb0752f523d9af603e8ffb27243f83af
Razmo99 <lewis.munro@live.com>
2021-06-16 12:52:49 +1000

    Added Group Match
    Added Group Match selection to the regex entry component.
    Changed the regex to use python re instead of tk re

--- Tenderizer/app_tk_widgets/components/regexentry.py
+++ Tenderizer/app_tk_widgets/components/regexentry.py
@@ -6,6 +6,7 @@ import tkinter.filedialog
 import tkinter as tk
 from tkinter.constants import CENTER
 import tkinter.ttk as ttk
+import re
 
 class RegexEntry(ttk.Labelframe):
     """Class that holds a entry box for regular expression with flags"""
@@ -13,10 +14,11 @@ class RegexEntry(ttk.Labelframe):
     def __init__(self,master):
         # Init the parent frame for this call that all widgets will sit in
         ttk.LabelFrame.__init__(self,master,text='Regular Expression')
-        self.grid(row=0,column=0,sticky='nsew',padx=5,pady=5)
+        self.grid(sticky='nsew',padx=5,pady=5)
         self.grid_rowconfigure(1,weight=1)
         self.grid_columnconfigure(0,weight=1)
 
+        self.compiled=None
         # Displays any re compile errors 
         self.statusdisplay = ttk.Label(self, text='')
         self.statusdisplay.grid(sticky='nwe',pady=5,padx=5,row=0,column=0,ipadx=5,ipady=5)
@@ -30,9 +32,23 @@ class RegexEntry(ttk.Labelframe):
 
         # This adds all the check boxes for re flags
         self.addoptions()
-    
-        # recomplile the regular expression every time a ket is pressed in the entry widget
-        self.entry.bind('<Key>', self.recompile)    
+        
+        # Frame to hold re group spinbox
+        self.group_frame = ttk.Frame(self)
+        self.group_frame.grid(sticky='nw',row=5,column=0)
+        self.group_frame.grid_rowconfigure(1,weight=1)
+        self.group_frame.grid_columnconfigure(0,weight=1)
+
+        self.group_label = ttk.Label(self.group_frame,text='RE Match Group:')
+        self.group_label.grid(row=0,column=0,sticky="nw",padx=5,pady=5)
+        # Spinbox that lets the user select the regex match group
+        self.group_var=tk.IntVar()
+        self.group=ttk.Spinbox(self.group_frame,from_=0,to=0,textvariable=self.group_var,state='readonly')
+        self.group.grid(row=0,column=1,sticky="nw",padx=5,pady=5)
+
+        # Recomplile the regular expression every time a key is pressed in the entry widget
+        self.entry.bind('<Key>', self.recompile)
+        self.recompile()
     
     def addoptions(self):
         """Adds re Flags under regex entry"""
@@ -68,13 +84,13 @@ class RegexEntry(ttk.Labelframe):
     def recompile(self, event=None):
         """Recompliles the Regular Expression and updates the statusdisplay"""
         try:
-            self.compiled = tk.re.compile(self.entry.get(),
+            self.compiled = re.compile(self.entry.get(),
                                        self.getflags())
             self.statusdisplay.config(text="", background=self.bg)
+            self.group.configure(to=self.compiled.groups)
         except tk.re.error as msg:
             self.compiled = None
             self.statusdisplay.config(
                     text=f"re.error: {str(msg)}",
                     background="red")
-        else:
-            pass
+            self.group.configure(to=0)

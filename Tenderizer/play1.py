9751564e94d8e829828639f48fc9c3ba24f41bb1
Razmo99 <lewis.munro@live.com>
2021-06-30 15:56:24 +1000

    using regex

--- Tenderizer/play1.py
+++ Tenderizer/play1.py
@@ -1,111 +1,74 @@
 import re
-
 import unittest
-from unittest.case import expectedFailure
 
-from stringarranger import StringArranger
 
+class RegexStringArranger():
+
+    def __init__(self,regular_expression) -> None:
+        self.regular_expression=regular_expression
     
-class TestStringArranger(unittest.TestCase):
+    def get_dup_str(self,string):
+        return re.finditer(
+            self.regular_expression,
+            string)
 
+    def remove_dup_chars(self,string,dup_chars):
+        str_len=len(string)
+        results=['_test']
+        re_matches=list(dup_chars)
+        re_matches_len=len(re_matches)
+        prev_span_end=None
+        for index_,re_match in enumerate(re_matches):
+
+            s_begin,s_end=re_match.span()
+            results.append(prev_span_end:behind)
+        return results
+
+class TestRegexStringArranger(unittest.TestCase):
+    
     def setUp(self):
-        self.string_arranger=StringArranger()
-        self.dup_chars=['-','_',' ','.']
+        pass
 
-    def test_get_dup_chars_begin_only(self):
-        self.string='__test'
-        self.expected_output=[1]
-        self.assertEqual(
-            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
-            self.expected_output)
+    def test_get_dup_chars_begin(self):
+        expression = re.compile(r"(\-|\_|\ |\.)\1+",flags=re.M|re.S)
+        re_arranger = RegexStringArranger(expression)
+        string='__test'
+        dup_str=list(re_arranger.get_dup_str(string))
+        self.assertEqual((0,2),dup_str[0].span())
+
+    def test_get_dup_chars_begin_end(self):
+        expression = re.compile(r"(\-|\_|\ |\.)\1+",flags=re.M|re.S)
+        re_arranger = RegexStringArranger(expression)
+        string='__test--'
+        dup_str=list(re_arranger.get_dup_str(string))
+        self.assertEqual((0,2),dup_str[0].span())        
+        self.assertEqual((6,8),dup_str[1].span())
+
+    def test_get_dup_chars_begin_mid_end(self):
+        expression = re.compile(r"(\-|\_|\ |\.)\1+",flags=re.M|re.S)
+        re_arranger = RegexStringArranger(expression)
+        string='__te---st--'
+        dup_str=list(re_arranger.get_dup_str(string))
+        self.assertEqual((0,2),dup_str[0].span())
+        self.assertEqual((4,7),dup_str[1].span())           
+        self.assertEqual((9,11),dup_str[2].span())
+
+    def test_remove_dup_chars_begin(self):
+        expression = re.compile(r"(\-|\_|\ |\.)\1+",flags=re.M|re.S)
+        re_arranger = RegexStringArranger(expression)
+        string='__test'
+        dup_str=list(re_arranger.get_dup_str(string))
+        de_dup_str=re_arranger.remove_dup_chars(string,dup_str)
+        self.assertEqual(['_test'],de_dup_str) 
+
+    def test_remove_dup_chars_end(self):
+        expression = re.compile(r"(\-|\_|\ |\.)\1+",flags=re.M|re.S)
+        re_arranger = RegexStringArranger(expression)
+        string='test__'
+        dup_str=list(re_arranger.get_dup_str(string))
+        de_dup_str=re_arranger.remove_dup_chars(string,dup_str)
+        self.assertEqual(['test_'],de_dup_str) 
+
     
-    def test_get_dup_chars_end_only(self):
-        self.string='test__'
-        self.expected_output=[5]
-        self.assertEqual(
-            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
-            self.expected_output)
-
-    def test_get_dup_chars_mid_only(self):
-        self.string='te__st'
-        self.expected_output=[3]
-        self.assertEqual(
-            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
-            self.expected_output)
-
-    def test_get_dup_chars_start_and_end_only(self):
-        self.string='__test__'
-        self.expected_output=[1,7]
-        self.assertEqual(
-            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
-            self.expected_output)            
-
-    def test_get_dup_chars_diff_chars(self):
-        self.string='__test--test..'
-        self.expected_output=[1,7,13]
-        self.assertEqual(
-            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
-            self.expected_output)     
-
-    def test_get_dup_chars_multiple_chars(self):
-        self.string='___test----test.....'
-        self.expected_output=[1,2,8,9,10,16,17,18,19]
-        self.assertEqual(
-            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
-            self.expected_output)
-
-    def test_get_non_dup_str_begin_only(self):
-        self.string='__test'
-        self.expected_output=['_','test']
-        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
-        self.assertEqual(
-            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
-            self.expected_output)
- 
-    def test_get_non_dup_str_end_only(self):
-        self.string='test__'
-        self.expected_output=['test_',]
-        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
-        self.assertEqual(
-            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
-            self.expected_output)
- 
-    def test_get_non_dup_str_end_only(self):
-        self.string='te__st'
-        self.expected_output=['te_','st']
-        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
-        self.assertEqual(
-            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
-            self.expected_output)
- 
-    def test_get_non_dup_str_start_and_end(self):
-        self.string='__test__'
-        self.expected_output=['_','test_']
-        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
-        self.assertEqual(
-            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
-            self.expected_output)
-
-    def test_get_non_dup_str_multiple_chars(self):
-        self.string='___test----test.....'
-        self.expected_output=['_','test-','test.']
-        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
-        self.assertEqual(
-            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
-            self.expected_output)
-
-    def test_get_non_dup_str_multiple_chars(self):
-        self.string='   Electrical Services   Lighting and Controls Refle/cted Ceiling Plan Level 5'
-        self.expected_output=[' ','Electrical Services ','Lighting and Controls Refle/cted Ceiling Plan Level 5']
-        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
-        self.assertEqual(
-            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
-            self.expected_output)
-
 if __name__ == '__main__':
-    from timeit import timeit
-    def job():
-        sa=StringArranger()
-        sa.get_duplicate_chars('  Elect  rical Services  Ligh  ting and Co  ntrols Refle/cted Ceiling Plan Level 5',['-','_',' ','.'])
-    print(timeit(lambda: job(),number=1_000_000))
-    #unittest.main()
+    unittest.main()
--- Tenderizer/stringarranger.py
+++ Tenderizer/stringarranger.py
@@ -1,3 +1,5 @@
+import unittest
+
 class StringArranger():
     
     def get_duplicate_chars(self,string,dup_chars):
@@ -28,3 +30,103 @@ class StringArranger():
         if dup_chars_array[-1] < str_len:
             results.append(string[dup_chars_array[-1]+1:])
         return results
+
+
+class TestStringArranger(unittest.TestCase):
+
+    def setUp(self):
+        self.string_arranger=StringArranger()
+        self.dup_chars=['-','_',' ','.']
+
+    def test_get_dup_chars_begin_only(self):
+        self.string='__test'
+        self.expected_output=[1]
+        self.assertEqual(
+            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
+            self.expected_output)
+    
+    def test_get_dup_chars_end_only(self):
+        self.string='test__'
+        self.expected_output=[5]
+        self.assertEqual(
+            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
+            self.expected_output)
+
+    def test_get_dup_chars_mid_only(self):
+        self.string='te__st'
+        self.expected_output=[3]
+        self.assertEqual(
+            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
+            self.expected_output)
+
+    def test_get_dup_chars_start_and_end_only(self):
+        self.string='__test__'
+        self.expected_output=[1,7]
+        self.assertEqual(
+            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
+            self.expected_output)            
+
+    def test_get_dup_chars_diff_chars(self):
+        self.string='__test--test..'
+        self.expected_output=[1,7,13]
+        self.assertEqual(
+            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
+            self.expected_output)     
+
+    def test_get_dup_chars_multiple_chars(self):
+        self.string='___test----test.....'
+        self.expected_output=[1,2,8,9,10,16,17,18,19]
+        self.assertEqual(
+            self.string_arranger.get_duplicate_chars(self.string,self.dup_chars),
+            self.expected_output)
+
+    def test_get_non_dup_str_begin_only(self):
+        self.string='__test'
+        self.expected_output=['_','test']
+        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
+        self.assertEqual(
+            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
+            self.expected_output)
+ 
+    def test_get_non_dup_str_end_only(self):
+        self.string='test__'
+        self.expected_output=['test_',]
+        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
+        self.assertEqual(
+            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
+            self.expected_output)
+ 
+    def test_get_non_dup_str_end_only(self):
+        self.string='te__st'
+        self.expected_output=['te_','st']
+        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
+        self.assertEqual(
+            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
+            self.expected_output)
+ 
+    def test_get_non_dup_str_start_and_end(self):
+        self.string='__test__'
+        self.expected_output=['_','test_']
+        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
+        self.assertEqual(
+            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
+            self.expected_output)
+
+    def test_get_non_dup_str_multiple_chars(self):
+        self.string='___test----test.....'
+        self.expected_output=['_','test-','test.']
+        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
+        self.assertEqual(
+            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
+            self.expected_output)
+
+    def test_get_non_dup_str_multiple_chars(self):
+        self.string='   Electrical Services   Lighting and Controls Refle/cted Ceiling Plan Level 5'
+        self.expected_output=[' ','Electrical Services ','Lighting and Controls Refle/cted Ceiling Plan Level 5']
+        dup_chars_array=self.string_arranger.get_duplicate_chars(self.string,self.dup_chars)
+        self.assertEqual(
+            self.string_arranger.get_non_dup_str(self.string,dup_chars_array),
+            self.expected_output)
+
+if __name__ == '__main__':
+    unittest.main()        

6bab181dedfc5a9df97fd654c381a9da47d51db3
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2021-07-02 18:55:30 +1000

    run tests sets wkdir

--- Tenderizer/run_tests.py
+++ Tenderizer/run_tests.py
@@ -1,7 +1,14 @@
 import unittest
-loader = unittest.TestLoader()
-start_dir = './test'
-suite = loader.discover(start_dir)
+from pathlib import Path
+from os import chdir
 
-runner = unittest.TextTestRunner()
-runner.run(suite)
+if __name__ == '__main__':
+    working_dir=Path(__file__).resolve().parent
+    chdir(working_dir)
+    
+    loader = unittest.TestLoader()
+    start_dir = './test'
+    suite = loader.discover(start_dir)
+
+    runner = unittest.TextTestRunner()
+    runner.run(suite)

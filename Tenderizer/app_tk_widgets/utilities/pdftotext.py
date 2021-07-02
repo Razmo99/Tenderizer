cafd5747775ab9b747c1e724e8d3cbf97fa0fb6e
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2021-07-02 18:54:44 +1000

    refactored pdftotext
    execute only processes files, not directories for input

--- Tenderizer/app_tk_widgets/utilities/pdftotext.py
+++ Tenderizer/app_tk_widgets/utilities/pdftotext.py
@@ -9,34 +9,26 @@ class PDFToText(object):
     
     def __init__(self,exe):
         self.exe = exe
-        if not isinstance(self.exe,Path):
-            raise TypeError("exe: Expecting PathLib.Path")
 
-    def execute(self,p,o=None):
+    def execute(self,input_path,output=None):
         """ Calls xpdf pdftotext as a subprocess"""
-        # Only accept a path Object
-        if not isinstance(p,Path):
-            raise TypeError("p: Expecting PathLib.Path")
-        if not isinstance(o,Path):
-            raise TypeError("o: Expecting PathLib.Path")                
         args=[f'{self.exe.resolve()}','','']
-        # Handles Individual file
-        if p.is_file():
+        if input_path.is_file():
             logger.debug('Converting a file')
             already_existed=False
             created_dir_structure=False
-            args[1]=f'{p}'
+            args[1]=f'{input_path}'
             # Handles if the output is a directory or file
-            if o:
-                if not o.parent.exists():
+            if output:
+                if not output.parent.exists():
                     created_dir_structure=True
-                    o.parent.mkdir(parents=True,exist_ok=True)
-                if o.exists():
+                    output.parent.mkdir(parents=True,exist_ok=True)
+                if output.exists():
                     already_existed=True
-                if o.suffix =='.pdf':
-                    args[2]=f'{o.resolve()}'.replace('.pdf','.txt')
-                elif o.suffix == '.txt':
-                    args[2]=f'{o.resolve()}'
+                if output.suffix =='.pdf':
+                    args[2]=f'{output.resolve()}'.replace('.pdf','.txt')
+                elif output.suffix == '.txt':
+                    args[2]=f'{output.resolve()}'
             pdftotext=Popen(
                 args
             )
@@ -45,38 +37,13 @@ class PDFToText(object):
                 logger.debug(pdftotext.returncode)
             else:
                 dump={
-                    'input_path': f'{p.resolve()}',
-                    'output_path': f'{o.resolve()}',
+                    'input_path': f'{input_path.resolve()}',
+                    'output_path': f'{output.resolve()}',
                     'already_existed': already_existed,
-                    'created_dir_structure': created_dir_structure if not created_dir_structure else f'{o.parent}',
+                    'created_dir_structure': created_dir_structure if not created_dir_structure else f'{output.parent}',
                     'return_code' : pdftotext.returncode
                 }
                 logging.info(
                     json.dumps(dump)
                 )
             return pdftotext.returncode
-
-        # Handles in the input is a directory
-        elif p.is_dir():
-            logger.debug('Converting a directory')
-            # Handles if the output directory was not provided
-            if not o.is_dir():
-                logger.warning('Ouput is not a directory')
-                o=Path(f'{p.parent}\\{p.name}_txt')
-                if not o.exists():
-                    logger.debug('Creating directory: '+o.name)
-                    o.mkdir()
-            elif not o.exists():
-                logger.warning(f'Creating dir {o}')
-                o.mkdir()
-            pdf_files=p.rglob(f'*.pdf')
-            for index,pdf_file in enumerate(pdf_files,start=1):
-                args[1]=f'{pdf_file}'
-                args[2]=f"{o}\\{pdf_file.name.replace('.pdf','.txt')}"
-                pdftotext=Popen(
-                    args
-                )
-                pdftotext.wait()
-                if not pdftotext.returncode == 0:
-                    logger.warning(f'pdftotext returned: {pdftotext.returncode}')
-            logger.debug(f'Processed {index} files')

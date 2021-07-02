d9060ce133fdc4c276b3b2c7ec27f1d571455a83
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2021-07-03 08:55:45 +1000

    added coverage report

--- Tenderizer/run_tests.py
+++ Tenderizer/run_tests.py
@@ -1,8 +1,11 @@
 import unittest
 from pathlib import Path
 from os import chdir
-
+import coverage
 if __name__ == '__main__':
+    cov=coverage.Coverage()
+    cov.start()
+
     working_dir=Path(__file__).resolve().parent
     chdir(working_dir)
     
@@ -12,3 +15,8 @@ if __name__ == '__main__':
 
     runner = unittest.TextTestRunner()
     runner.run(suite)
+
+    cov.stop()
+    cov.save()
+
+    cov.html_report()

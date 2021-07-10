d59c92eb5cc0d24784eec0f92395cbeb0d4e953f
Razmo99 <3089087+Razmo99@users.noreply.github.com>
2021-07-10 17:24:53 +1000

    changes to PDFToText execute method
    no longer checks if the file exists and just passes it on to pdftotext.exe. stderr and stdout are captured

--- Tenderizer/app_tk_widgets/utilities/pdftotext.py
+++ Tenderizer/app_tk_widgets/utilities/pdftotext.py
@@ -1,8 +1,9 @@
 import os
-from pathlib import Path
+from pathlib import Path,PurePath
 from subprocess import Popen
 import logging
 import json
+import subprocess
 logger = logging.getLogger(__name__)
 
 class PDFToText(object):
@@ -13,50 +14,48 @@ class PDFToText(object):
     def execute(self,input_path,output=None):
         """ Calls xpdf pdftotext as a subprocess"""
         args=[f'{self.exe.resolve()}','','']
-        if input_path.is_file():
-            logger.debug('Converting a file')
-            already_existed=False
-            created_dir_structure=False
-            args[1]=f'{input_path}'
-            # Handles if the output is a directory or file
-            if output:
-                if not output.parent.exists():
-                    created_dir_structure=True
-                    output.parent.mkdir(parents=True,exist_ok=True)
-                if output.exists():
-                    already_existed=True
-                if output.suffix =='.pdf':
-                    args[2]=f'{output.resolve()}'.replace('.pdf','.txt')
-                elif output.suffix == '.txt':
-                    args[2]=f'{output.resolve()}'
-            pdftotext=Popen(
-                args
+        already_existed=False
+        created_dir_structure=False
+        args[1]=f'{input_path.resolve()}'
+        if output:
+            if not output.parent.exists():
+                created_dir_structure=True
+                output.parent.mkdir(parents=True,exist_ok=True)
+            if output.exists():
+                already_existed=True
+            args[2]=PurePath(output.resolve()).with_suffix('.txt').__str__()
+        if output is None:
+            del args[2]
+        pdftotext=Popen(
+            args,
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE
+        )
+        pdftotext.wait()
+        if not pdftotext.returncode == 0:
+            if pdftotext.returncode == 1:
+                raise OpenPDFError(args)
+            elif pdftotext.returncode == 2:
+                raise OpenOutputFileError(args)
+            elif pdftotext.returncode == 3:
+                raise PDFPermissionsError(args)
+            elif pdftotext.returncode == 99:
+                raise XPDFTools(args)                                        
+        else:
+            dump={
+                'input_path': args[1],
+                'output_path': args[2] if len(args) == 3 else PurePath(args[1]).with_suffix('.txt').__str__(),
+                'already_existed': already_existed,
+                'created_dir_structure': created_dir_structure if not created_dir_structure else f'{output.parent}',
+                'return_code' : pdftotext.returncode
+            }
+            logger.info(
+                json.dumps(dump)
             )
-            pdftotext.wait()
-            if not pdftotext.returncode == 0:
-                if pdftotext.returncode == 1:
-                    raise OpenPDFError(args)
-                elif pdftotext.returncode == 2:
-                    raise OpenOutputFileError(args)
-                elif pdftotext.returncode == 3:
-                    raise PDFPermissionsError(args)
-                elif pdftotext.returncode == 99:
-                    raise XPDFTools(args)                                        
-            else:
-                dump={
-                    'input_path': f'{input_path.resolve()}',
-                    'output_path': f'{output.resolve()}',
-                    'already_existed': already_existed,
-                    'created_dir_structure': created_dir_structure if not created_dir_structure else f'{output.parent}',
-                    'return_code' : pdftotext.returncode
-                }
-                logging.info(
-                    json.dumps(dump)
-                )
-            return pdftotext.returncode
+        return pdftotext.returncode
 
 class XPDFTools(Exception):
-    """ Basic exception for erros raised by xpdf tools """
+    """ Basic exception for errors raised by xpdf tools """
     def __init__(self, arguments,msg=None) -> None:
         self.msg=msg if msg else 'Error with Xpdf Tools'
         super(XPDFTools,self).__init__(self.msg)
